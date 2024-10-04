96dbda47fdd29bebc07581bc264ea8659091e814
laxmanpradhan784 <laxmanpradhan784@gmail.com>
2024-10-04 16:11:13 +0530

    dffc

--- laravel-api/app/Http/Controllers/StudentController.php
+++ laravel-api/app/Http/Controllers/StudentController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
     //
+    function list(){
+        return Student::all();
+    }
 }
