f5f53533b03e351c321d4984e09a419b260269f0
laxmanpradhan784 <laxmanpradhan784@gmail.com>
2024-10-07 12:00:25 +0530

    new things add

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -4,4 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-Route::apiResource('students', StudentController::class);
+Route::get('/students', [StudentController::class, 'index']); // Get all students
+Route::get('/students/{id}', [StudentController::class, 'show']); // Get a specific student
+Route::post('/students', [StudentController::class, 'store']); // Create a new student
+Route::put('/students/{id}', [StudentController::class, 'update']); // Update a specific student
+Route::delete('/students/{id}', [StudentController::class, 'destroy']); // Delete a specific student
+
+?>
